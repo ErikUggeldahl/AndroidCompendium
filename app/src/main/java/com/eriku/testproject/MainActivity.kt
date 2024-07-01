5d027911a01d29b0d2a39ae91ac580ac07a4c59b
Erik Uggeldahl <erikuggeldahl@gmail.com>
2024-07-01 14:58:06 -0700

    Initial Rive addition

--- app/src/main/java/com/eriku/testproject/MainActivity.kt
+++ app/src/main/java/com/eriku/testproject/MainActivity.kt
@@ -11,9 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import app.rive.runtime.kotlin.RiveAnimationView
+import app.rive.runtime.kotlin.core.ExperimentalAssetLoader
 import com.eriku.testproject.ui.theme.TestProjectTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalAssetLoader::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,6 +27,15 @@ class MainActivity : ComponentActivity() {
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
+                    )
+                    AndroidView(
+                        factory = { context ->
+                            RiveAnimationView(context).apply {
+                                setRiveResource(
+                                    resId = R.raw.test,
+                                )
+                            }
+                        },
                     )
                 }
             }
