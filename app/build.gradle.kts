ca2dbe996cac240e1293abce9584274fac0ea173
Erik Uggeldahl <erikuggeldahl@gmail.com>
2024-07-01 23:57:55 -0700

    Add ViewModel and Retrofit with GSON for HTTP and JSON

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,10 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.startup)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
     implementation(libs.rive)
 
     implementation(libs.hilt)
